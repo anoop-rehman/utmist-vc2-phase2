05e9201fd463411131a39b1f8d4833248ea6ad01
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-04-04 12:13:54 -0400

    temp push, testing velocity..

--- ppo_demo.py
+++ ppo_demo.py
@@ -7,12 +7,12 @@ from stable_baselines3.common.env_util import make_vec_env
 vec_env = make_vec_env("Ant-v4", n_envs=4)
 
 model = PPO("MlpPolicy", vec_env, verbose=1)
-model.learn(total_timesteps=25000) # around 4 epochs
-model.save("ppo_ant")
+model.learn(total_timesteps=25000*10) # 25000 timesteps = around 4 epochs
+model.save("ppo_ant_vel_400epochs_3target_onlyvelreward")
 
 del model # remove to demonstrate saving and loading
 
-model = PPO.load("ppo_ant")
+model = PPO.load("ppo_ant_vel_400epochs_3target_onlyvelreward")
 
 obs = vec_env.reset()
 while True:

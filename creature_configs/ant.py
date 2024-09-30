102ef63f2b2dd03e5c4cd72c4e52aea71bd877e8
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-09-29 23:27:37 -0400

    Added seg 5 yipeeeee!

--- creature_configs/ant.py
+++ creature_configs/ant.py
@@ -82,7 +82,9 @@ class Ant(legacy_base.Walker):
             self._mjcf_root.find('geom', 'seg1_geom'),
             self._mjcf_root.find('geom', 'seg2_geom'),
             self._mjcf_root.find('geom', 'seg3_geom'),
-            self._mjcf_root.find('geom', 'seg4_geom')]
+            self._mjcf_root.find('geom', 'seg4_geom'),
+            self._mjcf_root.find('geom', 'seg5_geom'),
+            ]
 
   @composer.cached_property
   def actuators(self):

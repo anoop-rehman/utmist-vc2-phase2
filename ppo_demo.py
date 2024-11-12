033e3d083c303e163caad2b4099b94f7c8c46b00
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-11-11 20:37:27 -0500

    tried training for 2,500,000 timesteps; rowers still havent got past spawn.
    realized gear needs to be cranked way up. increased from 150 to 700.

--- ppo_demo.py
+++ ppo_demo.py
@@ -33,9 +33,11 @@ vec_env = make_vec_env('LocalAnt-v4', n_envs=4)
 
 model = PPO("MlpPolicy", vec_env, verbose=1)
 reward_logger = RewardLoggerCallback()
-model.learn(total_timesteps=250000, callback=reward_logger) # 25000 timesteps = around 4 epochs
+
 # model.learn(total_timesteps=2500, callback=reward_logger) 
-model.save("saved_models/ppo_twoArmRower_250000timesteps_vmax_v1")
+# model.learn(total_timesteps=2500000, callback=reward_logger) # 25000 timesteps = around 4 epochs
+
+# model.save("saved_models/ppo_twoArmRower_2500000timesteps_vmax_v1")
 
 # del model # remove to demonstrate saving and loading
 
@@ -45,7 +47,7 @@ model.save("saved_models/ppo_twoArmRower_250000timesteps_vmax_v1")
 # plt.title('Training Rewards Over Time')
 # plt.show()
 
-model.load("saved_models/ppo_twoArmRower_250000timesteps_vmax_v1")
+model.load("saved_models/ppo_twoArmRower_2500000timesteps_vmax_v1")
 
 obs = vec_env.reset()
 while True:
